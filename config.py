0659299a264305f83f42c8cb614d9b445df888a6
Akhil.A.S <119261400+wagger1@users.noreply.github.com>
2023-05-02 21:47:34 +0530

    Update config.py
    Signed-off-by: Akhil.A.S <119261400+wagger1@users.noreply.github.com>

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1739381637").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/wagger1/AnonXMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/wagger1/MusicX")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
